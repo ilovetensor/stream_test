a8710f44e648593d055f9a7755c3fcea36cde6b3
Declan Costello <dec1costello@gmail.com>
2023-08-29 18:54:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,9 @@ st.write('Hello world!')
 
 df = pd.read_csv("https://github.com/dec1costello/stream_test/blob/main/Vars.csv")
 
-with st.sidebar:
-    st.markdown('<h1 style="font-family: Consolas; font-size: 34px;">Select Your Inputs Here...</h1>', unsafe_allow_html=True)
-    options = df["EV"].dropna().tolist()
-    player = st.selectbox('Player', options)
+with st.sidebar(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+    # options = df["EV"].dropna().tolist()
+    # player = st.selectbox('Player', options)
+)
