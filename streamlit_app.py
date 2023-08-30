c48af75f5600e22f4c9364542e4a047a5d3eb428
Declan Costello <dec1costello@gmail.com>
2023-08-29 21:53:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-# model = joblib.load('xgbr.joblib')
+model = joblib.load('xgbr.joblib')
 
 st.title('Distance Predictor')
 
@@ -24,6 +24,6 @@ df['pull_percent'].iloc[0] = pull_percent
 
 st.dataframe(df)
 
-# p =  model.predict(df)
+p =  model.predict(df)
 
-# st.write(p[0])
+st.write(p[0])
