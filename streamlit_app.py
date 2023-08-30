cae775b0c369b747c1a608f2a934b4cd1cbcfe58
Declan Costello <dec1costello@gmail.com>
2023-08-29 20:27:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,16 +14,3 @@ fav_platoon_split_for_batter = st.selectbox("Good Platoon Split?", [0,1])
 game_elevation = st.slider("Choose Game Elevation",20,7349) 
 
 pull_percent =  st.slider("Choose Pull %",0,100)
-
-
-
-
-# with st.sidebar:
-#     add_radio = st.radio(
-#         "Choose a shipping method",
-#         ("Standard (5-15 days)", "Express (2-5 days)")
-#     )
-
-
-# options = df["EV"].dropna().tolist()
-# player = st.selectbox('Player', options)
