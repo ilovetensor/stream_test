4df681bffd534f85f8abf00cb80f4b69fa06a5dc
Declan Costello <dec1costello@gmail.com>
2023-08-29 19:15:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,26 @@ ev = st.sidebar.selectbox(
     (df['EV'])
 )
 
+
+
 st.write('Launch Angle:', la)
 
 st.write('Exit Velocity:', ev)
+
+
+
+
+passengerid = st.text_input("Input Passenger ID", '123456') 
+pclass = st.selectbox("Choose class", [1,2,3])
+name  = st.text_input("Input Passenger Name", 'John Smith')
+sex = st.select_slider("Choose sex", ['male','female'])
+age = st.slider("Choose age",0,100)
+sibsp = st.slider("Choose siblings",0,10)
+parch = st.slider("Choose parch",0,2)
+ticket = st.text_input("Input Ticket Number", "12345") 
+fare = st.number_input("Input Fare Price", 0,1000)
+cabin = st.text_input("Input Cabin", "C52") 
+embarked = st.select_slider("Did they Embark?", ['S','C','Q'])
 
 # with st.sidebar:
 #     add_radio = st.radio(
