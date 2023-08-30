76ace5b51ae50492bc1afaa63662ebf20dc1be17
Declan Costello <dec1costello@gmail.com>
2023-08-29 18:18:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
