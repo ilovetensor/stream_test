9ebc97f2aee1b21986a1dc4f4de0d821e01a4b0d
Declan Costello <dec1costello@gmail.com>
2023-08-29 21:21:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ ev = st.slider("Choose EV",0,100)
 release_speed = st.slider("Choose Release Speed",60,110)
 fav_platoon_split_for_batter = st.selectbox("Good Platoon Split?", [0,1])
 game_elevation = st.slider("Choose Game Elevation",20,7349) 
-pull_percent =  st.slider("Choose Pull %",0.0,1.0)
+#pull_percent =  st.slider("Choose Pull %",0.0,1.0)
 
 # df = pd.read_csv('final_input.csv')
 
