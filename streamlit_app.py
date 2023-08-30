8ade6dd7d8c1722c069a6601ecf0f25d735a545d
Declan Costello <dec1costello@gmail.com>
2023-08-29 18:21:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,10 @@ if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
 
-col1, col2 = st.columns(2)
+col1 = st.columns(1)
+
 
 with col1:
-    st.checkbox("Disable text input widget", key="disabled")
-    st.radio(
-        "Set text input label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-    )
-    st.text_input(
-        "Placeholder for the other text input widget",
-        "This is a placeholder",
-        key="placeholder",
-    )
-
-with col2:
     text_input = st.text_input(
         "Enter some text 👇",
         label_visibility=st.session_state.visibility,
