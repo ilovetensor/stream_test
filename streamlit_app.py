16b701151d4d4716708837dce7b700f732798e4e
Declan Costello <dec1costello@gmail.com>
2023-08-30 19:19:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,13 @@ import joblib
 
 st.title('Distance Predictor')
 
-la = st.slider("Launch Angle",0,50, index=25)
-ev = st.slider("Exit Velocity",0,120)
-release_speed = st.slider("Release Speed",60,110)
-fav_platoon_split_for_batter = st.slider("Favorable Batter Platoon Split", 0,1)
+la = st.slider("Launch Angle",0,50, value=25)
+ev = st.slider("Exit Velocity",0,120, value=60)
+release_speed = st.slider("Release Speed",60,110, value=85)
 game_elevation = st.slider("Game Elevation",20,7349) 
 pull_percent =  st.slider("Pull %",0.0,1.0)
+fav_platoon_split_for_batter = st.slider("Favorable Batter Platoon Split", 0,1, value = 1)
+
 
 df = pd.read_csv('final_input.csv')
 
